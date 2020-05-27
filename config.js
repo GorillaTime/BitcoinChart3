106732fc7f7fe3f0c5181403610b836a38beb025
Gorilla <log1230gori@outlook.jp>
2020-05-28 00:49:00 +0900

    a

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter:{
+    API_key:'2mrK7KN2dW8BKnytlVsulOyBP',
+    API_secret_key:'qBeygZgzQUqbppe99ILUHf2QkXEJtc2nwU0kFdu0oXith9valP',
+    token:'1224295757332639744-fd2coDG6MRDn691idFBHNltzko3Edj',
+    token_secret:'7r7wtkcgUFWDDVrcRy3HjIqCE5E9rQtx9iPPUQMEHln9N'
+  }
+}
